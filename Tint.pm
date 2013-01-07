52cc51331391c5bd0c15ad46dc57fa36642f4263
Ben Crowell <notreal@fakewillnotwork>
2013-01-07 08:45:33 -0800

    logins seem to be working

--- Tint.pm
+++ Tint.pm
@@ -713,6 +713,53 @@ function format_variable_name(x) {
                                                          ],
                                                'args' => []
                                              },
+          'check_if_cookies_enabled' => {
+                                          'text' => [
+                                                      [
+                                                        'lit',
+                                                        '<script type="text/javascript">
+    // based on code by balexandre, http://stackoverflow.com/questions/531393/how-to-detect-if-cookies-are-disabled-is-it-possible
+    function test_cookie_create(name, value, days) {
+        var expires;
+        if (days) {
+            var date = new Date();
+            date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+            expires = "; expires=" + date.toUTCString();
+        }
+        else expires = "";
+        document.cookie = name + "=" + value + expires + "; path=/";
+    }
+
+    function test_cookie_read(name) {
+        var nameEQ = name + "=";
+        var ca = document.cookie.split(\';\');
+        for (var i = 0; i < ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) == \' \') c = c.substring(1, c.length);
+            if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+        }
+        return null;
+    }
+
+    function test_cookie_erase(name) {
+        test_cookie_create(name, "", -1);
+    }
+
+    function test_cookies_enabled() {
+        var r = false;
+        test_cookie_create("testing", "Hello", 1);
+        if (test_cookie_read("testing") != null) {
+            r = true;
+            test_cookie_erase("testing");
+        }
+        return r;
+    }
+</script>
+'
+                                                      ]
+                                                    ],
+                                          'args' => []
+                                        },
           'checker.how_to_enter_answers' => {
                                               'text' => [
                                                           [
